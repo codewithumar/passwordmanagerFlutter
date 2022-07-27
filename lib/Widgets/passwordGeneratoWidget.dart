3ee8d0eb8863c1955115fb6c4144b64af2233e82
codewithumar <codewithumar@gmail.com>
2022-07-27 15:45:56 +0500

    Trello ID: hIn1Z11o

--- lib/Widgets/passwordGeneratoWidget.dart
+++ lib/Widgets/passwordGeneratoWidget.dart
@@ -1,5 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_seekbar/seekbar/seekbar.dart';
+import 'package:flutter/services.dart';
 
 class generatepassword extends StatefulWidget {
   const generatepassword({Key? key}) : super(key: key);
@@ -10,6 +12,10 @@ class generatepassword extends StatefulWidget {
 
 class _generatepasswordState extends State<generatepassword> {
   final controller = TextEditingController();
+  bool hasNumbers = false;
+  bool hasSpecial = false;
+  bool hasUpercase = false;
+  bool hasLowerCase = false;
   @override
   void dispose() {
     super.dispose();
@@ -18,9 +24,9 @@ class _generatepasswordState extends State<generatepassword> {
 
   @override
   Widget build(BuildContext context) {
-    double v = 10.0;
+    // double v = 10.0;
     return Container(
-      padding: const EdgeInsets.all(32.0),
+      padding: const EdgeInsets.all(15.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.center,
@@ -35,27 +41,89 @@ class _generatepasswordState extends State<generatepassword> {
           const SizedBox(
             height: 20.0,
           ),
+          Row(
+            children: [
+              Checkbox(
+                  value: hasNumbers,
+                  onChanged: (bool? newValue) {
+                    setState(() {
+                      hasNumbers = newValue!;
+                    });
+                  }),
+              const SizedBox(
+                width: 25.0,
+              ),
+              Checkbox(
+                  value: hasLowerCase,
+                  onChanged: (bool? newValue) {
+                    setState(() {
+                      hasLowerCase = newValue!;
+                    });
+                  }),
+              const SizedBox(
+                width: 30.0,
+              ),
+              Checkbox(
+                  value: hasUpercase,
+                  onChanged: (bool? newValue) {
+                    setState(() {
+                      hasUpercase = newValue!;
+                    });
+                  }),
+              const SizedBox(
+                width: 25.0,
+              ),
+              Checkbox(
+                  value: hasSpecial,
+                  onChanged: (bool? newValue) {
+                    setState(() {
+                      hasSpecial = newValue!;
+                    });
+                  }),
+            ],
+          ),
+          Row(
+            children: const [
+              Text('Numbers'),
+              SizedBox(
+                width: 10.0,
+              ),
+              Text('LowerCase'),
+              SizedBox(
+                width: 10.0,
+              ),
+              Text('UperCase'),
+              SizedBox(
+                width: 10.0,
+              ),
+              Text('Special'),
+            ],
+          ),
           TextField(
               readOnly: true,
               controller: controller,
+              enableInteractiveSelection: false,
               decoration: InputDecoration(
                   border: const OutlineInputBorder(),
                   suffixIcon: IconButton(
                     icon: const Icon(Icons.copy),
-                    onPressed: () {},
+                    onPressed: () {
+                      final data = ClipboardData(text: controller.text);
+                      Clipboard.setData(data);
+
+                      const snackbar = SnackBar(
+                        content: Text("Password Coppied"),
+                        backgroundColor: Colors.red,
+                      );
+
+                      ScaffoldMessenger.of(context)
+                        ..removeCurrentMaterialBanner()
+                        ..showSnackBar(snackbar);
+                    },
                   ))),
           const SizedBox(
             height: 20.0,
           ),
-          SizedBox(
-              width: 200,
-              child: SeekBar(
-                progresseight: 10,
-                indicatorRadius: 0.0,
-                value: v,
-                isRound: false,
-                progressColor: Colors.red,
-              )),
           Center(child: buildButton()),
         ],
       ),
@@ -72,6 +140,19 @@ class _generatepasswordState extends State<generatepassword> {
   }
 
   String generatePasswrod() {
-    return "abccccc";
+    final length = 20;
+    String capitalalphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    String smallalphabets = 'abcdefghijklmnopqrstuvwxyz';
+    String numbers = '0123456789';
+    String special = '!@#\$%^&*()_+';
+    String chars = '';
+    chars += hasUpercase == true ? capitalalphabets : "";
+    chars += hasNumbers == true ? numbers : "";
+    chars += hasLowerCase == true ? smallalphabets : "";
+    chars += hasSpecial == true ? special : "";
+    return List.generate(length, (index) {
+      final random = Random.secure().nextInt(chars.length);
+      return chars[random];
+    }).join('');
   }
 }
