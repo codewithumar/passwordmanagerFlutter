2539670fe22be959ef5d7fe3964cc803e91a456a
codewithumar <codewithumar@gmail.com>
2022-07-27 17:04:17 +0500

    trello Id: Jqet0XBV

--- lib/Widgets/passwordGeneratoWidget.dart
+++ lib/Widgets/passwordGeneratoWidget.dart
@@ -12,10 +12,12 @@ class generatepassword extends StatefulWidget {
 
 class _generatepasswordState extends State<generatepassword> {
   final controller = TextEditingController();
+  final controllerLength = TextEditingController();
   bool hasNumbers = false;
   bool hasSpecial = false;
   bool hasUpercase = false;
   bool hasLowerCase = false;
+
   @override
   void dispose() {
     super.dispose();
@@ -24,7 +26,6 @@ class _generatepasswordState extends State<generatepassword> {
 
   @override
   Widget build(BuildContext context) {
-    // double v = 10.0;
     return Container(
       padding: const EdgeInsets.all(15.0),
       child: Column(
@@ -94,7 +95,7 @@ class _generatepasswordState extends State<generatepassword> {
               ),
               Text('UperCase'),
               SizedBox(
-                width: 10.0,
+                width: 15.0,
               ),
               Text('Special'),
             ],
@@ -104,47 +105,100 @@ class _generatepasswordState extends State<generatepassword> {
               controller: controller,
               enableInteractiveSelection: false,
               decoration: InputDecoration(
+                  hintText: "Your Random Password will be here",
                   border: const OutlineInputBorder(),
                   suffixIcon: IconButton(
                     icon: const Icon(Icons.copy),
                     onPressed: () {
-                      final data = ClipboardData(text: controller.text);
-                      Clipboard.setData(data);
+                      if (controller.text == '') {
+                        buildsnackbar("Error");
+                      } else {
+                        final data = ClipboardData(text: controller.text);
+                        Clipboard.setData(data);
 
-                      const snackbar = SnackBar(
-                        content: Text("Password Coppied"),
-                        backgroundColor: Colors.red,
-                      );
-
-                      ScaffoldMessenger.of(context)
-                        ..removeCurrentMaterialBanner()
-                        ..showSnackBar(snackbar);
+                        buildsnackbar("Password Coppied");
+                      }
                     },
                   ))),
           const SizedBox(
             height: 20.0,
           ),
-          Center(child: buildButton()),
+          TextField(
+              controller: controllerLength,
+              enableInteractiveSelection: false,
+              inputFormatters: <TextInputFormatter>[
+                FilteringTextInputFormatter.allow(
+                    RegExp("^(?:[1-9]|[1-4][0-9]|50)\$")),
+              ],
+              decoration: const InputDecoration(
+                hintText: "Enter length of password between 8-50",
+                border: OutlineInputBorder(),
+              )),
+          Row(
+            children: [
+              buildButton(),
+              const SizedBox(
+                width: 20.0,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    controller.text = "";
+                  },
+                  child: const Text('Refresh')),
+            ],
+          ),
         ],
       ),
     );
   }
 
+  buildsnackbar(String message) {
+    final snackbar = SnackBar(
+      content: Text(message),
+      backgroundColor: Colors.red,
+    );
+
+    ScaffoldMessenger.of(context)
+      ..removeCurrentMaterialBanner()
+      ..showSnackBar(snackbar);
+  }
+
+  validate() {}
   Widget buildButton() {
     return ElevatedButton(
         child: const Text("Generate"),
         onPressed: () {
           final password = generatePasswrod();
           controller.text = password;
+          setState(() {});
         });
   }
 
+  // Widget dropdownlist() {
+  //   String? value;
+  //   final items = ['1', '2', '3', '4'];
+  //   return DropdownButton<String>(
+  //        value: value,
+  //       isExpanded: true,
+  //       items: items.map(builddropdown).toList(),
+  //       onChanged: (value) => setState(() => setState(() {
+  //             value = value;
+  //           })));
+  // }
+
+  // DropdownMenuItem<String> builddropdown(String item) => DropdownMenuItem(
+  //     value: item,
+  //     child: Text(
+  //       item,
+  //       style: const TextStyle(fontWeight: FontWeight.bold),
+  //     ));
+
   String generatePasswrod() {
-    final length = 20;
+    int length = int.parse(controllerLength.text);
     String capitalalphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     String smallalphabets = 'abcdefghijklmnopqrstuvwxyz';
     String numbers = '0123456789';
-    String special = '!@#\$%^&*()_+';
+    String special = '!@#\$%^&*()_+/.,`';
     String chars = '';
     chars += hasUpercase == true ? capitalalphabets : "";
     chars += hasNumbers == true ? numbers : "";
